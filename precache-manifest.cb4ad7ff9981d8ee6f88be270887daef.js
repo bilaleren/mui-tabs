bbb5a548b72b0aeb8ed57217cb537525ba4ef9e5
webnoi <bilal@webnoi.com>
2022-04-12 02:55:15 +0300

    Updates

---
+++ precache-manifest.cb4ad7ff9981d8ee6f88be270887daef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69eab8dee3686fb679dd274793dcb14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d114e7345f92907b81d6",
+    "url": "./static/css/2.90ff7994.chunk.css"
+  },
+  {
+    "revision": "1b5a49f4259b75eb4e0e",
+    "url": "./static/css/main.53b00c81.chunk.css"
+  },
+  {
+    "revision": "d114e7345f92907b81d6",
+    "url": "./static/js/2.391257c8.chunk.js"
+  },
+  {
+    "revision": "59ddb421a2367d8612f6e280fcb71001",
+    "url": "./static/js/2.391257c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5a49f4259b75eb4e0e",
+    "url": "./static/js/main.6e8c2113.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/main.6e8c2113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dc8be905aa662f7fa2",
+    "url": "./static/js/runtime-main.ac81ba21.js"
+  }
+]);
